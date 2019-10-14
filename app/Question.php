c9c18c159bdabcf0151c752f37895db0339f24b7
zigcBenx <zigac.benko@gmail.com>
2019-10-14 15:03:25 +0200

    Styling votes and views

--- app/Question.php
+++ app/Question.php
@@ -25,4 +25,14 @@ class Question extends Model
     public function getCreatedDateAttribute(){
     	return $this->created_at->diffForHumans(); // How many days/months/years ago
     }
+
+    public function getStatusAttribute(){
+        if ($this->answers >0){
+            if ($this->best_answer_id != null){
+                return 'answered-accepted';
+            }
+            return 'answered';
+        }
+        return 'unanswered';
+    }
 }
