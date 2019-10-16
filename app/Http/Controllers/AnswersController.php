eda88f6a624798fa6e1281a0cea7c5e9b12dd03b
zigcBenx <zigac.benko@gmail.com>
2019-10-16 07:14:09 +0200

    Updating answer functionality

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -34,9 +34,11 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+
+        return view('answers.edit',compact('question','answer'));
     }
 
     /**
@@ -46,9 +48,14 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update',$answer);
+        $answer->update($request->validate([
+            'body' => 'required',
+        ]));
+
+        return redirect()->route('questions.show',$question->slug)->with('success','Your answer has been successfully updated.');
     }
 
     /**
@@ -59,6 +66,6 @@ class AnswersController extends Controller
      */
     public function destroy(Answer $answer)
     {
-        //
+        $this->authorize('delete',$answer);
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,7 +6,9 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use Illuminate\Support\Facades\Gate;
 
 use App\Question;
+use App\Answer;
 use App\Policies\QuestionPolicy;
+use App\Policies\AnswerPolicy;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -16,6 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
     ];
 
     /**
