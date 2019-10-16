adce43d31663d7f20ffd5feab8207bcc7aed7a99
zigcBenx <zigac.benko@gmail.com>
2019-10-16 06:48:49 +0200

    Display answers for question

--- app/Answer.php
+++ app/Answer.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 //use App\User;
 class Answer extends Model
 {
+    protected $fillable = ['body','user_id'];
     public function question() {
     	return $this->belongsTo(Question::class);
     }
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionsController')->except('show');
 
+//Route::posts('/questions/{question}/answers', 'AnswersController@store')->name('answer.store');
+Route::resource('questions.answers', 'AnswersController')->except(['index','create','show']);
+
 Route::get('/questions/{slug}','QuestionsController@show')->name('questions.show');
