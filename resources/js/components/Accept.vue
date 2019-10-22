a85a28ad85e21e5d4e1ed98dc657fb0ffa53f8d2
zigcBenx <zigac.benko@gmail.com>
2019-10-22 20:17:33 +0200

    Addin answer acception to vue component

---
+++ resources/js/components/Accept.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+		 
+	    <a v-if="canAccept" title="Mark this answer as best answer" :class="classes" @click.prevent="create"><i class="fa fa-check fa-3x"></i>
+	    </a>
+	
+	    
+        <a v-if="accepted" title="The question owner accepted this answer as best answer" :class="classes"><i class="fa fa-check fa-3x"></i>
+        </a>
+    
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ['answer'],
+		data () {
+			return{
+				isBest: this.answer.is_best,
+				id: this.answer.id
+			}
+		},
+		computed: {
+			canAccept () {
+				return true;
+			},
+
+			accepted() {
+				return !this.canAccept && this.isBest;
+			},
+			classes()  {
+				return [
+					'mt-2',
+					this.isBest ? 'vote-accepted' : ''
+				]
+			},
+			endpoint() {
+				return `/answers/${this.id}/accept`;
+			}
+		},
+		methods: {
+			create(){
+				axios.post(this.endpoint)
+				.then(res => {
+					this.$toast.success(res.data.message, "Success", {
+						timeout: 3000,
+						position: 'bottomLeft'
+					});
+					this.isBest = true;
+				});
+			}
+		}
+	}
+</script>
--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -38,9 +38,7 @@
     @if ($model instanceof App\Question)
         <favorite :question="{{ $model }}"></favorite>
     @elseif ($model instanceof App\Answer)
-        @include('shared._accept', [
-            'model' => $model
-        ])
+        <accept :answer="{{ $model }}"></accept>
     @endif
 
 </div>
